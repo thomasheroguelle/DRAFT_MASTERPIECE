7b5167e407328b41fd04e1dfc992720f11947697
Thomas HEROGUELLE <totosh93270@gmail.com>
2024-01-08 15:49:16 +0100

    add css for logement-detail

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -16,27 +16,27 @@ export class LoginComponent {
     private http: HttpClient,
   ) {}
 
-  // Login() {
-  //   console.log(this.email);
-  //   console.log(this.password);
+  Login() {
+    console.log(this.email);
+    console.log(this.password);
 
-  //   let bodyData = {
-  //     email: this.email,
-  //     password: this.password,
-  //   };
+    let bodyData = {
+      email: this.email,
+      password: this.password,
+    };
 
-  //   this.http
-  //     .post('http://localhost:8091/api/v1/user/login', bodyData)
-  //     .subscribe((resultData: any) => {
-  //       console.log(resultData);
+    this.http
+      .post('http://localhost:8091/api/v1/user/login', bodyData)
+      .subscribe((resultData: any) => {
+        console.log(resultData);
 
-  //       if (resultData.message == 'Email not exits') {
-  //         alert('Email not exits');
-  //       } else if (resultData.message == 'Login Success') {
-  //         this.router.navigateByUrl('/logements');
-  //       } else {
-  //         alert('Incorrect Email and Password not match');
-  //       }
-  //     });
-  // }
+        if (resultData.message == 'Email not exits') {
+          alert('Email not exits');
+        } else if (resultData.message == 'Login Success') {
+          this.router.navigateByUrl('/logements');
+        } else {
+          alert('Incorrect Email and Password not match');
+        }
+      });
+  }
 }
